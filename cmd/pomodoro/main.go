49544694b13bb5460caf1f09583a10f4a30de789
Chris Booty <chrisalan313@gmail.com>
2024-02-21 21:03:08 -0500

    Use rivo/tview for terminal ui

--- cmd/pomodoro/main.go
+++ cmd/pomodoro/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"context"
 	"github.com/chrisalan313/pomodoro/internal/ui"
+	"time"
 )
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	ui.Run(ctx)
+
+	// Amount of time we have for this pomodoro
+	// Make this something we can pass in later
+	t := 5 * time.Second
+	ui.Run(ctx, time.Now().Add(t))
 }
--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -2,7 +2,45 @@ package ui
 
 import (
 	"context"
+	"time"
+
+	"github.com/rivo/tview"
 )
 
-func Run(ctx context.Context) {
+var (
+	view *tview.Modal
+	app  *tview.Application
+)
+
+func Run(ctx context.Context, t time.Time) {
+	app = tview.NewApplication()
+	view = tview.NewModal().
+		SetText("REPLACE THIS WITH TIME COUNT DOWN").
+		AddButtons([]string{"Quit", "Cancel"}).
+		SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+			if buttonLabel == "Quit" {
+				app.Stop()
+			}
+		})
+	tick := time.Tick(500 * time.Millisecond)
+
+	go eventLoop(ctx, t, tick)
 }
+
+func eventLoop(ctx context.Context, dl time.Time, tick <-chan time.Time) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-tick:
+			updateTime(dl)
+			// call func that updates UI to reflect new time
+		}
+	}
+}
+
+func updateTime(dl time.Time) {
+	// func that updates UI with new string
+}
+
+// func that makes new string given time left
