dddd577794ec0f1b25b09974883992e1ad80f775
Chris Booty <chrisalan313@gmail.com>
2024-02-23 17:05:10 -0500

    Add proper countdown with each previous time replaced inline

--- cmd/pomodoro/main.go
+++ cmd/pomodoro/main.go
@@ -2,21 +2,18 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/chrisalan313/pomodoro/internal/ui"
 )
 
 func main() {
-	fmt.Println("Entered main")
-	defer fmt.Println("Exited main")
 	ctx, canc := context.WithCancel(context.Background())
 	defer canc()
 
 	// Amount of time we have for this pomodoro
 	// Make this something we can pass in later
-	t := 5 * time.Second
+	t := 25 * time.Minute
 
 	ui.Run(ctx, time.Now().Add(t))
 }
--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -8,25 +8,19 @@ import (
 
 // Run initializes the application's main logic with a specified context, time, and cancel function.
 func Run(ctx context.Context, t time.Time) {
-	fmt.Println("Entered Run")      // Prints a message indicating the start of the Run function
-	defer fmt.Println("Exited Run") // Ensures that a message indicating the exit of the Run function is printed at the end
 
-	ticker := time.NewTicker(500 * time.Millisecond) // Creates a new ticker that ticks every 500 milliseconds
-	defer ticker.Stop()                              // Ensures that the ticker is stopped when the function exits
+	ticker := time.NewTicker(time.Second) // Creates a new ticker that ticks every 500 milliseconds
+	defer ticker.Stop()                   // Ensures that the ticker is stopped when the function exits
 
-	fmt.Println("Set tick")     // Prints a message indicating that the ticker has been set
 	eventLoop(ctx, t, ticker.C) // Starts the eventLoop function in a new goroutine
 }
 
 // eventLoop continuously checks for events such as tick events or context cancellation.
 func eventLoop(ctx context.Context, dl time.Time, tick <-chan time.Time) {
-	fmt.Println("Entered eventLoop")      // Prints a message indicating the start of the eventLoop function
-	defer fmt.Println("Exited eventLoop") // Ensures that a message indicating the exit of the eventLoop function is printed at the end
 
 	for {
 		select {
 		case <-tick:
-			fmt.Println("tick happened")
 			tl := time.Until(dl) // Calculates the duration until the specified deadline
 			if tl < 0 {          // Checks if the deadline has passed
 				return
@@ -38,8 +32,8 @@ func eventLoop(ctx context.Context, dl time.Time, tick <-chan time.Time) {
 
 // updateTime prints the remaining time until a specified deadline.
 func updateTime(tl time.Duration) {
-	fmt.Println("Entered updateTime")            // Prints a message indicating the start of the updateTime function
-	st := tl.String()                            // Converts the duration to a string
-	fmt.Println("Duration until deadline: ", st) // Prints the remaining duration until the deadline
-	fmt.Println("Exited updateTime")             // Prints a message indicating the exit of the updateTime function
+	tl = tl.Truncate(time.Second)
+	st := tl.String()
+	fmt.Printf("\033[A\033[2K")
+	fmt.Println("\rTime left: ", st) // Prints the remaining duration until the deadline
 }
